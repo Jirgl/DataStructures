0d839400b97929d57f5fbbaa212a7a6bbdb88495
Jakub Jirgl <jirgl.jakub@gmail.com>
2016-02-03 20:08:25 +0100

    added new localizations

--- l10n/translations/en-US.js
+++ l10n/translations/en-US.js
@@ -12,5 +12,5 @@ bobrilRegisterTranslations("en-US", [
 "The linked list items can be stored anywhere in a computer's memory. Each item contains data and reference to the next (or previous) item. The list items can thus be dynamically added or removed without the need for any reallocation of memory.",
 "The item of doubly linked list contains data and two references to previous and next item.",
 "The queue is a specific data structure which inserts items at the end of the list and takes it back from its beginning.",
-"The stack is a specific data structure which inserts items at the begin (or the end) of the list and takes it back from its beginning (or end)."
+"The stack is a specific data structure which inserts items at the beginning (or the end) of the list and takes it back from its beginning (or end)."
 ]);
