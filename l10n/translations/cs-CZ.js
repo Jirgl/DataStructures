bf5ff22a63aaa31eece31a3efa2ef69d3a502dc4
Jakub Jirgl <jirgl.jakub@gmail.com>
2016-02-10 20:36:42 +0100

    updated localizations

--- l10n/translations/cs-CZ.js
+++ l10n/translations/cs-CZ.js
@@ -8,7 +8,7 @@ bobrilRegisterTranslations("cs-CZ", [
 "Dvousměrně zřetězený seznam",
 "Fronta",
 "Zásobník",
-"přijde...",
+"Pole je struktura, která obsahuje maximálně n prvků. Maximální počet (n) prvků musí být definován již při alokaci během inicializace pole. Z tohoto důvodu jsou pole vhodná pro ukládání dat statického charakteru. Pokud by alokované místo nestačilo a hrozilo by přetečení, je nutné alokovat nové pole o vetší velikosti. Data ze starého pole překopírovat do nového a následně uvolnit paměť alokovanou starým polem. Výhodou polí je rychlý přístup k datům na konkrétní pozici (indexu).",
 "Položky zřetězených seznamů mohou být uloženy kdekoli v paměti počítače a každá položka obsahuje data a referenci na následující (případně předcházející) položku. Položky seznamu je tak možné dynamicky přidávat nebo odebírat bez nutnosti jakékoli realokace v paměti.",
 "Položka dvousměrně zřetězeného seznamu obsahuje data a dvě reference na předchozí a následující prvek seznamu.",
 "Fronta je specifická datová struktura, která vkládá prvky na konec seznamu a odebírá je ze svého začátku.",
