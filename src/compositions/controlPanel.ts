6e54c9506f4c889ed258afdf51ecbc08106a3ab6
Jakub Jirgl <jirgl.jakub@gmail.com>
2016-05-13 19:49:06 +0200

    updated layout

--- src/compositions/controlPanel.ts
+++ src/compositions/controlPanel.ts
@@ -1,4 +1,5 @@
 import * as b from 'bobril';
+import * as m from 'bobril-m';
 
 export interface IControlPanelData {
     actions: b.IBobrilNode;
@@ -22,16 +23,29 @@ function getBlock(children: b.IBobrilNode, left: number, bottom: number = 0): b.
     };
 }
 
+function getLine(): b.IBobrilNode {
+    return {
+        tag: 'div',
+        style: {
+            height: 1,
+            backgroundImage: '-webkit-gradient(linear, 0 0, 80% 0, from(' + m.primary1Color() + '), to(' + m.transparent + '))',
+        }
+    }
+}
+
 export function create(data: IControlPanelData): b.IBobrilNode {
     return {
         tag: 'div',
-            style: { position: 'relative', height: 80 },
-            children: [
-                getBlock(data.actions, 0, 10),
-                getBlock(data.options, 90, 10),
-                getBlock(data.valueBox, 200),
-                getBlock(data.submitButton, 420)
-            ]
-        
+        style: {
+            position: 'relative',
+            height: 80
+        },
+        children: [
+            getLine(),
+            getBlock(data.actions, 0, 10),
+            getBlock(data.options, 90, 10),
+            getBlock(data.valueBox, 200),
+            getBlock(data.submitButton, 420, 2)
+        ]
     };
 }
