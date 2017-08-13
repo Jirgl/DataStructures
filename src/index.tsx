b8a3662bab22bb8bd2877267f655c452b51af86d
jirgl <jirgl.jakub@gmail.com>
2017-08-13 15:52:26 +0200

    Fix tap event

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,10 @@
 ﻿import * as React from 'react';
 import { AppContainer } from 'react-hot-loader';
+import * as injectTapEventPlugin from 'react-tap-event-plugin';
 import * as ReactDOM from 'react-dom';
 import { App } from './app';
+
+injectTapEventPlugin();
 
 const render = (Component: any) => {
   ReactDOM.render(
