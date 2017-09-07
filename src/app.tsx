698366474d63893f25a2b4f42782f1fcd0861fd3
jirgl <jirgl.jakub@gmail.com>
2017-09-07 20:26:34 +0200

    Add custom theme

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { lightBlue500 } from 'material-ui/styles/colors';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import { NotFound } from './notFound';
 import { Header } from './components/header';
 import { ArrayPlaygrounds } from './playgrounds/arrayPlaygrounds/arrayPlaygrounds';
@@ -8,12 +10,17 @@ import { ListPlaygrounds } from './playgrounds/listPlaygrounds/listPlaygrounds';
 import { TreePlaygrounds } from './playgrounds/treePlaygrounds/treePlaygrounds';
 
 const navigationItems = ['arrays', 'lists', 'trees', 'heaps'];
+const muiTheme = getMuiTheme({
+    palette: {
+        primary1Color: lightBlue500
+    }
+});
 
 export interface AppProps { }
 
 export class App extends React.Component<AppProps, undefined> {
     render() {
-        return <MuiThemeProvider>
+        return <MuiThemeProvider muiTheme={muiTheme}>
             <BrowserRouter>
                 <div>
                     <Header title='Data Structures Playground' navigationItems={navigationItems} />
